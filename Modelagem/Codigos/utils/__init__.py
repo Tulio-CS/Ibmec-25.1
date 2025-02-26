4cbe97a7eae309f335848458f1282ee9e9da1d65
Tulio Castro Silva <tulio.castro14@gmail.com>
2025-02-26 18:05:15 -0300

    .

---
+++ Modelagem/Codigos/utils/__init__.py
@@ -0,0 +1,1 @@
+from .derivacaoNumerica import diferencaCentradaPrimeiraOrdem, diferencaCentradaSegundaOrdem, diferencaCentradaTerceiraOrdem, diferencaCentradaQuartaOrdem
--- Modelagem/Codigos/utils/derivacaoNumerica.py
+++ Modelagem/Codigos/utils/derivacaoNumerica.py
@@ -1,4 +1,15 @@
 
 
-def diferencialFinita(func,x,h=0.01):
-    return (func(x+h)-func(x))/h
+def diferencaCentradaPrimeiraOrdem(func:lambda x: expression,delta:float=1e-6)-> lambda x: expression:
+    return lambda x: (func(x + delta) - func(x - delta))/(2*delta)
+
+def diferencaCentradaSegundaOrdem(func:lambda x: expression,delta:float=1e-6)-> lambda x: expression:
+    return lambda x: (func(x + delta) - 2*func(x) + func(x - delta))/(delta**2)
+
+def diferencaCentradaTerceiraOrdem(func:lambda x: expression,delta:float=1e-6)-> lambda x: expression:
+    return lambda x: (func(x + 2*delta) - 2*func(x + delta) + 2*func(x - delta) - func(x - 2*delta))/(2*delta**3)
+
+def diferencaCentradaQuartaOrdem(func:lambda x: expression,delta:float=1e-6)-> lambda x: expression:
+    return lambda x: (func(x + 2*delta) - 4*func(x + delta) + 6*func(x) - 4*func(x - delta) + func(x - 2*delta))/(delta**4)
+
+
